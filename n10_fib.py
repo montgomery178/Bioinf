4bf24807320d6fbc52119235883cd1cb8fb808d7
Рузанна Цикуниб <113126727+montgomery178@users.noreply.github.com>
2022-12-13 23:03:21 +0300

    Update n10_fib.py

--- n10_fib.py
+++ n10_fib.py
@@ -1,18 +1,13 @@
 import itertools
 
-first_6_fibs = [1, 1, 2, 3, 5, 8]
- 
-r=range(6)
-i = iter(r)
-
 class Fib:    
     """По объектам этого класса можно итерироваться и получать 6 чисел Фибоначчи"""
 
     class _Fib_iter:
         """Внутренний класс — итератор"""
         def __init__(self):
-            self.i = 0
-            l=[0,1]
+            self.i = 1
+            l=[1,0]
             self.fibs = l
         
         def __next__(self):
